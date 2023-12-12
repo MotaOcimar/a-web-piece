aa11debe780df6da177dba9228499b2886fcc4bf
Ocimar Santos <29007115+MotaOcimar@users.noreply.github.com>
2023-12-12 16:28:09 +0000

    Cria a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Vegeta sobre o Amor entre Ocimar e Domis:</h1>
+      <img src="https://i.imgur.com/rnpPMcW.gif" />
+    </div>
+  );
+};
+
+export default Home;
